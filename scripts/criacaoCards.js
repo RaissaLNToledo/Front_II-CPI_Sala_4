bf66602fa41d6f6d350bab0f3f9e95e8ad4013a7
RaissaLNToledo <raissaltoledo@yahoo.com.br>
2023-03-14 23:45:20 -0300

    Pequenas alterações nos nomes dos campos e inserção do reset

--- scripts/criacaoCards.js
+++ scripts/criacaoCards.js
@@ -8,10 +8,10 @@ function criarCards(array, elementoPai) {
             let divCard = document.createElement("div");
             divCard.setAttribute("class", "card");
             divCard.innerHTML = `
-                <h2>"${filme.titulo}"</h2>
+                <h2>${filme.titulo}</h2>
                 <img src="${filme.img}">
                 <p>
-                "${filme.descricao}
+                ${filme.descricao}
                 </p>
             `;
             elementoPai.appendChild(divCard);
@@ -24,4 +24,6 @@ let elementoPai = document.getElementById("paiCards");
 
 btnSalvar.addEventListener("click", function(){
     criarCards(listaObjetos, elementoPai);
+    document.getElementById("formulario").reset()
+    console.log(listaObjetos);
 })
